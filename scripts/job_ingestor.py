b3db029101f6820184e0ee687fbff5946ea4d303
prajwalhaniya <prajwalhaniya@gmail.com>
2025-09-02 23:04:24 +0530

    improve job_ingestor

--- scripts/job_ingestor.py
+++ scripts/job_ingestor.py
@@ -1,12 +1,17 @@
 import requests
 import time
+from datetime import datetime, timedelta, timezone
+
+def get_current_time_plus_two_minutes():
+    current_time = datetime.now(timezone.utc)
+    new_time = current_time + timedelta(minutes=2)
+    return new_time.isoformat().replace("+00:00", "Z")
+
+url = "http://localhost:3000/create-job"
+headers = {"Content-Type": "application/json"}
 
 def create_api_call_mocks(num_jobs: int):
-    url = "http://localhost:3000/create-job"
-    headers = {"Content-Type": "application/json"}
-
     for i in range(num_jobs):
-        # unique idempotency key using timestamp + counter
         idempotency_key = f"{int(time.time() * 1000)}_{i}"
 
         payload = {
@@ -21,7 +26,7 @@ def create_api_call_mocks(num_jobs: int):
             "timeout_sec": 60,
             "idempotency_key": idempotency_key,
             "tenant_id": "tenant_123",
-            "run_at": "2025-08-29T17:34:02Z"
+            "run_at": get_current_time_plus_two_minutes()
         }
 
         try:
@@ -37,7 +42,30 @@ def create_function_call_mocks(num_jobs: int):
     for i in range(num_jobs):
         py_fn = f"print('python function {i}')"
         node_fn = f"console.log('node function {i}')"
+        
+        idempotency_key = f"{int(time.time() * 1000)}_{i}"
+
+        payload = {
+            "job_type": "py_fn_call" if i % 2 == 0 else "js_fn_call",
+            "payload": py_fn if i % 2 == 0 else node_fn,
+            "queue_id": 1,
+            "priority": 5,
+            "max_attempts": 3,
+            "t"
+            "imeout_sec": 60,
+            "idempotency_key": idempotency_key,
+            "tenant_id": "tenant_123",
+            "run_at": get_current_time_plus_two_minutes()
+        }
+
+        try:
+            response = requests.post(url, headers=headers, json=payload)
+            print(f"Job {i+1}/{num_jobs}: Status {response.status_code}, Response: {response.text}")
+        except requests.exceptions.RequestException as e:
+            print(f"Job {i+1}/{num_jobs} failed: {e}")
+
+
 
 if __name__ == "__main__":
     # create_api_call_mocks(100)
-    create_function_call_mocks(100)
+    create_function_call_mocks(500)
